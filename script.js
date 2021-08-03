53ec01a94bf83c4510b2c262ecb361f0f785df58
BuildTools <unconfigured@null.spigotmc.org>
2021-08-03 14:29:06 +0700

    final design

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
+
+window.addEventListener('load', function () {
+    document.getElementById("defaultOpen").click();
+})
+
 var serverStatus = new XMLHttpRequest();
 serverStatus.open('GET', 'https://api.mcsrvstat.us/2/tomakosmp.ddns.net', true);
-
-
-
 serverStatus.onload = function (e) {
     const resp = JSON.parse(serverStatus.response);
     if (resp.online == true)  document.getElementById("visitor-action").style.display = "initial";
@@ -10,3 +12,24 @@ serverStatus.onload = function (e) {
 };
 
 serverStatus.send();
+
+function platform(evt, platformName) {
+    // Declare all variables
+    var i, tabcontent, tablinks;
+  
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+  
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+  
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    document.getElementById(platformName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
